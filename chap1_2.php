44231c82bf9f766c609a7eb979b1de0d7ddc58bb
miii3 <ankokomi0116@gmail.com>
2021-03-20 11:35:43 +0900

    first commit

---
+++ chap1_2.php
@@ -0,0 +1,13 @@
+<?php
+var_dump("上底は？");
+$top = trim(fgets(STDIN));
+var_dump($top);
+var_dump("下底は？");
+$bottom = trim(fgets(STDIN));
+var_dump($bottom);
+var_dump("高さは？");
+$height = trim(fgets(STDIN));
+var_dump($height);
+
+
+var_dump("面積は". (($top + $bottom) * $height)/2);
