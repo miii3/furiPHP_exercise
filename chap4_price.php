d220bf5fdc3d82457569c082da5b725255b5deb3
miii3 <ankokomi0116@gmail.com>
2021-04-12 18:42:44 +0900

    課題完了

---
+++ chap4_price.php
@@ -0,0 +1,20 @@
+<?php
+function add($item1, $item2, $item3) {
+    return $item1 + $item2 + $item3;
+}
+
+function totalTax($sum) {
+    return floor($sum * 1.1);
+}
+
+function displayMsg($name, $sum, $price) {
+    $msg = <<<EOM
+{$name}様
+ご注文承りました。
+function displayMsg($name, $sum, $price) {
+合計{$sum}円です。
+{$price}円(税込)になります。\n
+EOM;
+    echo $msg;
+}
+
